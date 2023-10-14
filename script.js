2ab06bd199ea409208656ed19ec0fc862a59891a
pavithramk18 <pavithram.kumaresan@gmail.com>
2023-10-14 18:19:16 +0530

    all tasks done

--- script.js
+++ script.js
@@ -6,8 +6,21 @@ console.log("JavaScript - Day -3: JS Array & objects");
 
 //Solution
 
+let object1 = { name: "person1", age: 5 };
+let object2 = { age: 5, name: "person1" };
 
+// Convert JSON objects to strings
+let jsonString1 = JSON.stringify(object1);
+let jsonString2 = JSON.stringify(object2);
 
+// Sort the strings to ensure property order doesn't matter
+let sortedJSON1 = jsonString1.split('').sort().join('');
+let sortedJSON2 = jsonString2.split('').sort().join('');
 
-
+// Compare the sorted strings
+if (sortedJSON1 === sortedJSON2) {
+  console.log("The JSON objects are equivalent without considering property order.");
+} else {
+  console.log("The JSON objects are not equivalent without considering property order.");
+}
 
